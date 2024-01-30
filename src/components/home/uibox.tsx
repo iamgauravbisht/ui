269171473567789bb9b37f75db3e8ac95343904a
Gaurav <gauravbisht154@gmail.com>
2024-01-30 22:50:25 +0530

    dark theme removed

--- src/components/home/uibox.tsx
+++ src/components/home/uibox.tsx
@@ -54,7 +54,7 @@ export default function Uibox() {
       </div>
       <button
         type="button"
-        className="w-4/5 text-center text-nowrap text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 my-3 hover:border-white hover:border-2"
+        className="w-4/5 text-center text-nowrap text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 shadow-lg shadow-purple-500/50 font-medium rounded-lg text-sm px-5 py-2.5 my-3 hover:border-white hover:border-2"
         onClick={showModal}
       >
         See UI
